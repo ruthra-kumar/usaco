d0038eac5e33486c41f704a43089f0be2fe88713
ruthra kumar <ruthrab@gmail.com>
2017-03-13 10:05:31 +0530

    Update subset_sum.cpp

--- subset_sum.cpp
+++ subset_sum.cpp
@@ -1,88 +1,51 @@
+/*
+ID  : ruthrab1
+TASK: subset
+PROG: subset
+LANG: C++
+*/
+
 #include <iostream>
-#include <vector>
+#include <fstream>
 
 using namespace std;
 
-int n,partition_sum ;
-vector< vector<int> >   partitions;
-
-bool Found(vector<int> Lst,int Val)
-{
-    for(unsigned int i=0;i<Lst.size();i++)
-    {
-        if(Lst[i]   ==  Val)    return  true;
-    }
-    return false;
-}
-
-bool Partition_found(vector<int> L)
-{
-    for(unsigned int i=0;i<partitions.size();i++)
-    {
-    }
-}
-
-void    Print(vector<int> Ls)
-{
-    cout    <<  "{ ";
-    for(unsigned int i=0;i<Ls.size();i++)
-    {
-        cout    <<  Ls[i]   << " ";
-    }
-    cout    <<  "} ";
-}
-
-void    AddElement(int  Sum,int Start,vector<int> left_partition)
-{
-    if(!Found(left_partition,Start))
-    {
-        left_partition.push_back(Start);
-        Sum +=  Start;
-
-        if(Sum  ==  partition_sum)
-        {
-
-
-            vector<int> right_partition;
-            //Generate right partition based on what's left in left partition
-            for(int i=1;i<=n;i++)
-            {
-                if(!Found(left_partition,i))
-                {
-                    right_partition.push_back(i);
-                }
-            }
-
-            Print(left_partition);
-            cout    <<  " and " ;
-            Print(right_partition);
-            cout    << '\n';
-
-        }
-    }
-    for(int Next=Start+1;Next<=n;Next++)
-    {
-        AddElement(Sum,Next,left_partition);
-    }
-}
-
 int main()
 {
-    cin >>  n;
-    if((n*(n+1))%4  !=  0)
+    fstream fin("subset.in",ios::in);
+    int n;
+    fin >>  n;
+
+    fin.close();
+
+    fstream fout("subset.out",ios::out);
+
+    int partition_sum   =   (n*(n+1))/4;
+    if( (n*(n+1))%4  == 0)
     {
-        cout    <<  0   <<  '\n';
+        int *F  =   new int[partition_sum+1];
+
+        for(int i=0;i<(partition_sum+1);i++)    F[i]    =   0;
+
+        F[1]    =   1;
+
+        for(int i=2;i<=n;++i)
+        {
+            for(int j=partition_sum;j>0;--j)
+            {
+                if(j-i  >   0)
+                {
+                    F[j]   =    F[j]    +   F[j-i];
+                }
+            }
+        }
+        fout    <<  F[partition_sum]    <<  '\n';
+        delete[] F;
     }
     else
     {
-        partition_sum   =   (n*(n+1))/4;
-        vector<int> left;
-        for(int i=1;i<=n;i++)
-        {
-            AddElement(0,i,left);
-        }
+        fout    <<  "0\n";
     }
-
-
+    fout.close();
     return 0;
 }
